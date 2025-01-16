617368b781c7b554f5e916b090378bce216b000d
PIO <piodjiele@gmail.com>
2025-01-16 15:27:39 +0100

    ajout des algorithmes pour les autres formats

--- mot_de_passe.py
+++ mot_de_passe.py
@@ -4,7 +4,7 @@ import py7zr
 import rarfile
 import string
 import time
-start_time=time.time()
+
 def force_brute(file ,ensemble, max_length):
     if file.endswith(".zip"):
         try:
@@ -31,7 +31,54 @@ def force_brute(file ,ensemble, max_length):
             print(f"Erreur :{e}")
         except Exception as e :
             print (f"Erreur inattendu : {e}")
-    
+    if file.endswith(".rar"):
+        try:
+            rf = rarfile.RarFile(file)
+            print("Recherche du mot de passe pour le fichier .rar commencée.")
+        
+            for length in range(1, max_length + 1):
+                print(f"Test des mots de passe de longueur {length}")
+                for password in itertools.product(ensemble, repeat=length):
+                    password = ''.join(password)
+                    try:
+                        rf.extractall(pwd=password)
+                        print(f"Mot de passe trouvé : {password}")
+                        return True
+                    except rarfile.BadRarFile:
+                        print("Le fichier .rar est invalide ou corrompu.")
+                        return False
+                    except rarfile.RarWrongPassword:
+                        pass  # Continuer la recherche si le mot de passe est incorrect
+            print("Mot de passe non trouvé dans les combinaisons générées.")
+        except FileNotFoundError:
+            print("Fichier introuvable.")
+        except Exception as e:
+            print(f"Erreur : {e}")
+        return False
+    if file.endswith(".7z"):
+        try:
+            print("Recherche du mot de passe pour le fichier .7z commencée.")
+        
+            for length in range(1, max_length + 1):
+                print(f"Test des mots de passe de longueur {length}")
+                for password in itertools.product(ensemble, repeat=length):
+                    password = ''.join(password)
+                    try:
+                        with py7zr.SevenZipFile(file, mode='r', password=password) as zf:
+                            zf.extractall()
+                            print(f"Mot de passe trouvé : {password}")
+                            return True
+                    except py7zr.Bad7zFile:
+                        print("Le fichier .7z est invalide ou corrompu.")
+                        return False
+                    except py7zr.PasswordError:
+                        pass  # Continuer la recherche si le mot de passe est incorrect
+            print("Mot de passe non trouvé dans les combinaisons générées.")
+        except FileNotFoundError:
+            print("Fichier introuvable.")
+        except Exception as e:
+            print(f"Erreur : {e}")
+        return False
 
 
 
